6eb4e410c1f411e2a6036252179c443299d3c769
Поляков Роман <r.polyakov@etpgpb.ru>
2024-08-10 20:22:25 +0300

    iter11

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -17,17 +17,38 @@ func NewDB(DBPath string) *RDB {
 		log.Fatal(err)
 	}
 
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS urls(id int, short text not null, original text not null)")
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
 	return &RDB{
 		db: db,
 	}
 }
 
 func (d *RDB) Save(originalURL string, shortURL string) error {
-	panic("No implement")
+	_, err := d.db.Exec("INSERT INTO urls(short, original) VALUES ($1, $2)", shortURL, originalURL)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (d *RDB) Get(inputURL string) (string, error) {
-	panic("No implement")
+	var short, original string
+
+	row := d.db.QueryRowContext(context.Background(), "SELECT short, original FROM urls WHERE short = $1 or original = $1", inputURL)
+	if err := row.Scan(&short, &original); err != nil {
+		return "", err
+	}
+
+	if short != "" {
+		return short, nil
+	}
+
+	return original, nil
 }
 
 func (d *RDB) Ping(ctx context.Context) error {
