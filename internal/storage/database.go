8a9b0c9fe7773bded459bfe86a370fed3d230423
Поляков Роман <r.polyakov@etpgpb.ru>
2024-08-11 14:35:09 +0300

    iter11 Добавил функционал для использования БД в качестве хранилища ссылок

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"log"
 )
@@ -17,17 +19,46 @@ func NewDB(DBPath string) *RDB {
 		log.Fatal(err)
 	}
 
+	createUrlsTableQuery := `CREATE TABLE IF NOT EXISTS urls(
+		id serial primary key,
+		short_url varchar(255) not null,
+		original_url varchar(255) not null
+		)`
+	_, err = db.Exec(createUrlsTableQuery)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
 	return &RDB{
 		db: db,
 	}
 }
 
 func (d *RDB) Save(originalURL string, shortURL string) error {
-	panic("No implement")
+	_, err := d.db.Exec("INSERT INTO urls(short_url, original_url) VALUES ($1, $2)", shortURL, originalURL)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (d *RDB) Get(inputURL string) (string, error) {
-	panic("No implement")
+	var short, original string
+
+	row := d.db.QueryRowContext(context.Background(), "SELECT short_url, original_url FROM urls WHERE short_url = $1 or original_url = $1", inputURL)
+	if err := row.Scan(&short, &original); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", nil
+		}
+		return "", fmt.Errorf("cannot scan row: %w", err)
+	}
+
+	if short != "" {
+		return short, nil
+	}
+
+	return original, nil
 }
 
 func (d *RDB) Ping(ctx context.Context) error {
